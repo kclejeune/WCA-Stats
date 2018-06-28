159c53e599c442b1d8be75e6a5a3eaf516846aa1
Kennan LeJeune <kc.lejeune@gmail.com>
2018-06-28 14:59:28 -0500

    remove extra files

--- query.sql
+++
@@ -1,5 +0,0 @@
-SELECT personId, personName, COUNT(DISTINCT(competitionId)) as numComps FROM Results
-WHERE LEFT(personId, 4) = '2016'
-GROUP BY personId, personName
-ORDER BY numComps DESC
-LIMIT 100;
