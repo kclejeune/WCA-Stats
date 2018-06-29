14d72ea45fac8374b982124e570ac950044c87dd
Kennan LeJeune <kc.lejeune@gmail.com>
2018-06-29 05:26:01 -0500

    fix markdown table formatting in output file

--- MostCompetitionsByYear/runQuery.sh
+++ MostCompetitionsByYear/runQuery.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 password=$(cat ~/sqlPass.txt)
-for (( year=2003; year <= 2018; year++ ))
+for (( year=2003; year <= 2008; year++ ))
 do
     echo "\n## $year\n"
     echo "SELECT personId, personName, COUNT(DISTINCT(competitionId)) as numComps FROM Results
 WHERE LEFT(personId, 4) = $year
 GROUP BY personId, personName
 ORDER BY numComps DESC
-LIMIT 100;" | mysql -t -u root --password=$password wca | ghead -n -1 | gtail -n +2
-done > output.md
+LIMIT 5;" | mysql -t -u root --password=$password wca | ghead -n -1 | gtail -n +2
+done | sed 's/+/|/g' > output.md
