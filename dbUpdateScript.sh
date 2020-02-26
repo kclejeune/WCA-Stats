97d93618d4a98318f8b6f6901ce10b8cffe89eb8
Kennan LeJeune <kennan@case.edu>
2020-02-26 13:44:12 -0500

    update the db import script, add some caching to reuse recent .sql downloads

--- dbUpdateScript.sh
+++ dbUpdateScript.sh
@@ -1,24 +1,31 @@
-if [ -e wcaExport.sql.zip ]; then
+# always remove lingering zip archives before we proceed
+if [[ -e wcaExport.sql.zip ]]; then
     rm wcaExport.sql.zip
 fi
-if [ -e WCA_Export.sql ]; then
+
+# remove and redownload the file if the current export is more than 1 day old
+if [[ $(find "WCA_export.sql" -mtime +1 -print) ]]; then
     rm WCA_Export.sql
+    echo "Downloading Current WCA Export"
+    curl -# -o wcaExport.sql.zip https://www.worldcubeassociation.org/results/misc/WCA_export.sql.zip
+    # extract the sql dump
+    unzip wcaExport.sql.zip WCA_export.sql && rm wcaExport.sql.zip
 fi
 
-echo "Downloading Current WCA Export"
-curl -# -o wcaExport.sql.zip https://www.worldcubeassociation.org/results/misc/WCA_export.sql.zip
+# specify a .sqlPass with root user password and nothing else, otherwise prompt for the password
+if [[ -e .sqlPass ]]; then
+    pass=$(cat .sqlPass)
+else
+    read -s -p "MySQL Password: " pass
+fi
 
-# extract the sql dump
-unzip wcaExport.sql.zip WCA_export.sql
-
-# i'm not dumb enough to put a plaintext password on github
-pass=$(cat .sqlPass.txt)
-
+echo "Importing Database"
 # import sql dump
 mysql -u root --password=$pass wca < WCA_export.sql
 
+echo "Database import complete. Removing archived export files."
+
 # clean up files, leave sql dump file until the next update
 rm wcaExport.sql.zip
-rm WCA_export.sql
 clear
-echo Updated $(date) >> .wcaUpdateLog.txt
+echo Updated $(date) >> .updateLog
